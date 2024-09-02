998f9a71f2c63be0043281f5c71bc99553beb999
Jinwon Pyo <gch99517@gmail.com>
2024-09-02 07:50:00 +0000

    Add untouched smoothquant llaam source, start from here

---
+++ examples/llama3_local_smoothquant_ppl_eval.py
@@ -0,0 +1,33 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import torch
+
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+module_path = os.path.join(current_dir, '..', 'models/quantized/llama')
+sys.path.append(module_path)
+
+from smoothquant_modeling_llama import SqLlamaForCausalLM
+
+from transformers import AutoTokenizer
+
+import ppl_evaluator
+
+remote_model_id = 'meta-llama/Meta-Llama-3-8B'
+local_model_id = './pretrained_weights/vanilla_llama3_8b_fp16'
+
+model = SqLlamaForCausalLM.from_pretrained(local_model_id, torch_dtype=torch.float16, device_map='auto')
+
+from datasets import load_dataset
+
+tokenizer = AutoTokenizer.from_pretrained(remote_model_id)
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
+evaluator = ppl_evaluator.Evaluator(dataset, tokenizer, 'cuda')
+
+ppl = evaluator.evaluate(model)
+print(f"Local Smoothquant Llama3-8B (int8) perplexity: {ppl}")
+
