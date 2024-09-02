c869a6759e26d35149875a7e9fddf6209846f2c9
Jinwon Pyo <gch99517@gmail.com>
2024-09-02 06:39:32 +0000

    Add evaluation for vanilla llama3-8B

---
+++ examples/llama3_remote_vanilla_ppl_eval.py
@@ -0,0 +1,27 @@
+import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import torch
+
+from transformers.models.llama.modeling_llama import (
+  LlamaForCausalLM,
+)
+
+from transformers import AutoTokenizer
+
+import ppl_evaluator
+
+model_id = 'meta-llama/Meta-Llama-3-8B'
+
+model = LlamaForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map='auto')
+
+from datasets import load_dataset
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
+evaluator = ppl_evaluator.Evaluator(dataset, tokenizer, 'cuda')
+
+ppl = evaluator.evaluate(model)
+print(f"Original Llama3-8B (fp16) perplexity: {ppl}")
