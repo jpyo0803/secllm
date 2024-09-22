2e4d5f5a2ae8b758d9f9d892196530ee8ebb7e92
Jinwon Pyo <gch99517@gmail.com>
2024-09-22 09:39:33 +0000

    Add cpp Tensor

--- secllm/secllm_unittest.cpp
+++ secllm/secllm_unittest.cpp
@@ -7,6 +7,10 @@
 #include <gtest/gtest.h>
 #include <cstdint>
 #include <numeric>
+#include "tensor.h"
+#include <memory>
+
+using namespace std;
 
 TEST(GenerateMultKeyTest, RelativelyPrimeTest) {
     EXPECT_EQ(1, 1);
@@ -18,11 +22,62 @@ TEST(GenerateMultKeyTest, RelativelyPrimeTest) {
     }
 }
 
+TEST(TensorTest, TensorTest) {
+    vector<int> shape{2, 3, 4};
+    jpyo0803::Tensor<float> tensor(shape);
 
+    auto ret_shape = tensor.shape();
+    for (int i = 0; i < shape.size(); ++i) {
+        EXPECT_EQ(ret_shape[i], shape[i]);
+    }
+
+    vector<int> shape2 = {3, 4, 5};
+    vector<int> data2(shape2[0] * shape2[1] * shape2[2]);
+    for (int i = 0; i < data2.size(); ++i) {
+        data2[i] = i;
+    }
+    std::shared_ptr<jpyo0803::Tensor<int>> tensor2 = std::make_shared<jpyo0803::Tensor<int>>(shape2, data2);
+    EXPECT_EQ(tensor2.use_count(), 1);
+    auto tensor3 = tensor2;
+    EXPECT_EQ(tensor2.use_count(), 2);
+    EXPECT_EQ(tensor3.use_count(), 2);
+    auto tensor4 = tensor3;
+    EXPECT_EQ(tensor2.use_count(), 3);
+    EXPECT_EQ(tensor3.use_count(), 3);
+    EXPECT_EQ(tensor4.use_count(), 3);
+    auto tensor5 = tensor2;
+    EXPECT_EQ(tensor2.use_count(), 4);
+    EXPECT_EQ(tensor3.use_count(), 4);
+    EXPECT_EQ(tensor4.use_count(), 4);
+    EXPECT_EQ(tensor5.use_count(), 4);
+    auto tensor6 = tensor3;
+    EXPECT_EQ(tensor2.use_count(), 5);
+    EXPECT_EQ(tensor3.use_count(), 5);
+    EXPECT_EQ(tensor4.use_count(), 5);
+    EXPECT_EQ(tensor5.use_count(), 5);
+    EXPECT_EQ(tensor6.use_count(), 5);
+
+    EXPECT_TRUE(*tensor2 == *tensor6);
+    EXPECT_EQ(data2.size(), tensor5->num_elements());
+
+    tensor3.reset();
+    EXPECT_EQ(tensor2.use_count(), 4);
+    EXPECT_EQ(tensor4.use_count(), 4);
+    EXPECT_EQ(tensor5.use_count(), 4);
+    EXPECT_EQ(tensor6.use_count(), 4);
+    tensor4.reset();
+    EXPECT_EQ(tensor2.use_count(), 3);
+    EXPECT_EQ(tensor5.use_count(), 3);
+    EXPECT_EQ(tensor6.use_count(), 3);
+    tensor5.reset();
+    EXPECT_EQ(tensor2.use_count(), 2);
+    EXPECT_EQ(tensor6.use_count(), 2);
+    tensor6.reset();
+    EXPECT_EQ(tensor2.use_count(), 1);
+}
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
-
 
   int ret = RUN_ALL_TESTS();
   std::cout << "Unittest " << (ret == 0 ? "passed" : "failed") << std::endl;
