7bc30e91db36a5d74b1a168cbddfcc3bca9fe26d
Jinwon Pyo <gch99517@gmail.com>
2024-10-01 06:45:43 +0000

    Add always on assert

--- secllm_cpp/macro.h
+++ secllm_cpp/macro.h
@@ -1,13 +1,17 @@
+#ifndef SECLLM_MACRO_H
+#define SECLLM_MACRO_H
+
 #include <cstdlib>  // For std::abort()
+#include <iostream>
 
 // Custom assert that always checks, even in release mode
-#define ASSERT_ALWAYS(cond, msg)                         \
-    do {                                                 \
-        if (!(cond)) {                                   \
-            std::cerr << "Assertion failed: " << msg     \
-                      << " in file " << __FILE__         \
-                      << ", function " << __func__       \
-                      << ", line " << __LINE__ << '\n';  \
-            std::abort();                                \
-        }                                                \
-    } while (0)
+#define ASSERT_ALWAYS(cond, msg)                                               \
+  do {                                                                         \
+    if (!(cond)) {                                                             \
+      std::cerr << "Assertion failed: " << msg << " in file " << __FILE__      \
+                << ", function " << __func__ << ", line " << __LINE__ << '\n'; \
+      std::abort();                                                            \
+    }                                                                          \
+  } while (0)
+
+#endif  // SECLLM_MACRO_H
